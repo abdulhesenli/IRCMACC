63267a1bd78190537bc79109cba15f55a3c74b64
kenan <abdulhsnlik@bk.ru>
2024-12-06 14:52:16 +0400

    testimonials update

--- src/components/Testimonials/testimonials.jsx
+++ src/components/Testimonials/testimonials.jsx
@@ -1,7 +1,6 @@
-
 import '../../header.css';
 import './testimonials.css';
-import '../../res.css'
+import '../../res.css';
 import Comma from '../../assets/comma.svg';
 import RedCircle from '../../assets/RedCircle.svg';
 import WhiteComma from '../../assets/WhiteComma.svg';
@@ -12,33 +11,34 @@ import { useState } from 'react';
 
 function Testimonials() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [touchStartX, setTouchStartX] = useState(0);
+  const [touchEndX, setTouchEndX] = useState(0);
 
   const testimonials = [
     {
       name: 'Name Surname',
       title: 'Student',
-      text: 'loe industry standard dummy text ever sintype and scrambled it to make a type specimen book.',
+      text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
     },
     {
       name: 'Name Surname',
       title: 'Student',
-      text: 'ndustry standard dummy text ever since the 1500s, when an unknown ed it to make a type specimen book.',
-    },
-
-    {
-      name: 'Name Surname',
-      title: 'Student',
-      text: 'lorem Ipsum ithe 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      text: 'Lorem Ipsum has been the industry standard dummy text ever since the 1500s.',
     },
     {
       name: 'Name Surname',
       title: 'Student',
-      text: 'lorem Ipsum is simply  Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      text: 'When an unknown printer took a galley of type and scrambled it to make a type specimen book.',
     },
     {
       name: 'Name Surname',
       title: 'Student',
-      text: 'lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      text: 'Lorem Ipsum is simply dummy text with its roots in classical Latin literature.',
+    },
+    {
+      name: 'Name Surname',
+      title: 'Student',
+      text: 'Lorem Ipsum has survived not only five centuries, but also the leap into electronic typesetting.',
     },
   ];
 
@@ -50,8 +50,32 @@ function Testimonials() {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + testimonials.length) % testimonials.length);
   };
 
+  const handleTouchStart = (e) => {
+    setTouchStartX(e.touches[0].clientX);
+  };
+
+  const handleTouchMove = (e) => {
+    setTouchEndX(e.touches[0].clientX);
+  };
+
+  const handleTouchEnd = () => {
+    if (touchStartX - touchEndX > 50) {
+      handleNext(); // Sağdan sola kaydırma
+    } else if (touchEndX - touchStartX > 50) {
+      handlePrev(); // Soldan sağa kaydırma
+    }
+    setTouchStartX(0);
+    setTouchEndX(0);
+  };
+
   return (
-    <div id="TestsBG">
+    <div
+      id="TestsBG"
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+      style={{ userSelect: 'none' }}
+    >
       <h1 className="TestimonialsTitle">Testimonials</h1>
       <p className="TestimonialsText">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis massa nisl, porta ac erat quis,
@@ -59,7 +83,6 @@ function Testimonials() {
       </p>
 
       <div id="Testimonials" className="row" style={{ display: 'flex', justifyContent: 'center' }}>
-        {/* 3 Kartın Görünmesini Sağlama */}
         <div className="col-4" style={{ width: '400px' }}>
           <div className="TestimonialsCard">
             <img src={Comma} alt="Comma" className="Comma" />
@@ -89,6 +112,7 @@ function Testimonials() {
           <h6 className="CardTitle">{testimonials[(currentIndex + 2) % testimonials.length].name}</h6>
           <h6 className="CardText">{testimonials[(currentIndex + 2) % testimonials.length].title}</h6>
         </div>
+
         <div className="carousel-controls">
         <a href="#" onClick={(e) => { e.preventDefault(); handlePrev(); }}>
           <img src={ArrowCircleLeft} alt="ArrowCircleLeft" className="ArrowCircleLeft" />
@@ -97,17 +121,8 @@ function Testimonials() {
           <img src={ArrowCircleRight} alt="ArrowCircleRight" className="ArrowCircleRight" />
         </a>
       </div>
-      </div>
 
-      {/* Sağ ve Sol Oklar */}
-      {/* <div className="carousel-controls">
-        <a href="#" onClick={(e) => { e.preventDefault(); handlePrev(); }}>
-          <img src={ArrowCircleLeft} alt="ArrowCircleLeft" className="ArrowCircleLeft" />
-        </a>
-        <a href="#" onClick={(e) => { e.preventDefault(); handleNext(); }}>
-          <img src={ArrowCircleRight} alt="ArrowCircleRight" className="ArrowCircleRight" />
-        </a>
-      </div> */}
+      </div>
 
       <img src={RedCircle} alt="RedCircle" className="RedCircle" />
       <img src={WhiteComma} alt="WhiteComma" className="WhiteComma" />
